8a2e056ed0198e22961926630ae933dbf56c5df3
Manoj Kumar G <82995823+imanojg-hub@users.noreply.github.com>
2022-03-30 17:34:10 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,61 @@
+const QUOTEBANK = [
+  {
+    quote: "So many books, so little time.",
+    author: "- Frank Zappa",
+  },
+  {
+    quote: "Be yourself; everyone else is already taken.",
+    author: "- Oscar Wilde",
+  },
+  {
+    quote: "You only live once, but if you do it right, once is enough.",
+    author: "- Mae West",
+  },
+  {
+    quote: "Be the change that you wish to see in the world.",
+    author: "- Mahatma Gandhi",
+  },
+  {
+    quote:
+      "In three words I can sum up everything I've learned about life: it goes on.",
+    author: "- Robert Frost",
+  },
+  {
+    quote: "If you tell the truth, you don't have to remember anything.",
+    author: "- Mark Twain",
+  },
+  {
+    quote: "A friend is someone who knows all about you and still loves you.",
+    author: "- Elbert Hubbard",
+  },
+  {
+    quote: "We accept the love we think we deserve.",
+    author: "- Stephen Chbosky",
+  },
+  {
+    quote: "Without music, life would be a mistake.",
+    author: "- Friedrich Nietzsche",
+  },
+  {
+    quote:
+      "To be yourself in a world that is constantly trying to make you something else is the greatest accomplishment.",
+    author: "- Ralph Waldo Emerson",
+  },
+];
+
+window.onload = load;
+function load() {
+  generateQuote();
+}
+
+function generateQuote() {
+  let randomNumber = Math.floor(Math.random() * QUOTEBANK.length);
+  let randomData = QUOTEBANK[randomNumber];
+
+  let twitterLink =
+    "https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text=%22Your%20time%20is%20limited%2C%20so%20don%E2%80%99t%20waste%20it%20living%20someone%20else%E2%80%99s%20life.%22%20Steve%20Jobs";
+
+  document.getElementById("tweet-quote").href = twitterLink;
+  document.getElementById("text").innerText = randomData.quote;
+  document.getElementById("author").innerText = randomData.author;
+}
